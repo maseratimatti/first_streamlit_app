72f47d5551b569cf2b2d9c452248b1feb02864ab
maseratimatti <80660424+maseratimatti@users.noreply.github.com>
2023-01-30 14:30:42 +0100

    Update streamlit_app.py
    tilføjede emoji og ekstra menu ting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit 
 
-streamlit.title('My Mum"'"s New Healthy Diner')
+streamlit.title('My Mums New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
